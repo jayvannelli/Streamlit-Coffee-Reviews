55d52e9212ccc5eb4f23ac53c10a17d14ed474ae
jayvannelli <jaymvannelli@gmail.com>
2023-01-20 04:04:23 -0500

    Improve page design flow.

--- pages/country.py
+++ pages/country.py
@@ -7,19 +7,23 @@ def main():
 
     coffee_reviews = get_data()
 
-    left_column, right_column = st.columns(2)
-    with left_column:
-        origin_country = st.selectbox(label="Select origin country of beans:",
+    origin_tab, roaster_tab = st.tabs([
+        "Origin Country of coffee beans", "County of Roaster"
+    ])
+
+    with origin_tab:
+        origin_country = st.selectbox(label="Select origin country of coffee beans:",
                                       options=coffee_reviews['origin_1'].unique())
-    with right_column:
+
+        origin_country_df = coffee_reviews.loc[coffee_reviews['origin_1'] == origin_country]
+        st.dataframe(origin_country_df)
+
+    with roaster_tab:
         roaster_country = st.selectbox(label="Select country of roaster:",
                                        options=coffee_reviews["loc_country"].unique())
 
-    origin_country_df = coffee_reviews.loc[coffee_reviews['origin_1'] == origin_country]
-    roaster_country_df = coffee_reviews.loc[coffee_reviews['loc_country'] == roaster_country]
-
-    st.write(origin_country_df)
-    st.write(roaster_country_df)
+        roaster_country_df = coffee_reviews.loc[coffee_reviews['loc_country'] == roaster_country]
+        st.dataframe(roaster_country_df)
 
 
 if __name__ == "__main__":
