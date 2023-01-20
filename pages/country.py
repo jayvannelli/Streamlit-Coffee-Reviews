9f8e5802911cceea0a92a03f8a4b5a40085d5350
jayvannelli <jaymvannelli@gmail.com>
2023-01-20 02:23:53 -0500

    Clean up file.

--- pages/country.py
+++ pages/country.py
@@ -9,9 +9,11 @@ def main():
 
     left_column, right_column = st.columns(2)
     with left_column:
-        origin_country = st.selectbox("Select origin country of beans:", options=coffee_reviews['origin_1'].unique())
+        origin_country = st.selectbox(label="Select origin country of beans:",
+                                      options=coffee_reviews['origin_1'].unique())
     with right_column:
-        roaster_country = st.selectbox("Select country of roaster:", options=coffee_reviews["loc_country"].unique())
+        roaster_country = st.selectbox(label="Select country of roaster:",
+                                       options=coffee_reviews["loc_country"].unique())
 
     origin_country_df = coffee_reviews.loc[coffee_reviews['origin_1'] == origin_country]
     roaster_country_df = coffee_reviews.loc[coffee_reviews['loc_country'] == roaster_country]
